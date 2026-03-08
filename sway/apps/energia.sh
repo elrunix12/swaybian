2890d0df88a9a6655df6f3155316173dd29c53b5
elrunix12 <projetos_github@outlook.com>
2026-03-07 21:17:02 -0300

    atualiza script de energia para usar ícones da Font Awesome e ajusta configurações de bordas e espaçamento

--- sway/apps/energia.sh
+++ sway/apps/energia.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# Cria a lista de opções
-opcoes="⏻  Desligar\n  Reiniciar\n󰒲  Suspender\n󰗽  Sair do Sway"
+# Cria a lista de opções com ícones da Font Awesome
+opcoes="  Desligar\n  Reiniciar\n  Suspender\n  Sair do Sway"
 
 # Mostra as opções no Wofi e guarda a escolha
 escolha=$(echo -e "$opcoes" | wofi --show dmenu --prompt "Energia:" --width 250 --height 230)
 
 # Executa o comando baseado na escolha
 case "$escolha" in
-    "⏻  Desligar") systemctl poweroff ;;
-    "  Reiniciar") systemctl reboot ;;
-    "󰒲  Suspender") systemctl suspend ;;
-    "󰗽  Sair do Sway") swaymsg exit ;;
+    "  Desligar") systemctl poweroff ;;
+    "  Reiniciar") systemctl reboot ;;
+    "  Suspender") systemctl suspend ;;
+    "  Sair do Sway") swaymsg exit ;;
 esac
